e51ef2e93a95d68f3f93e816b0b5ac28caad1705
stijnana <stijn.olthof@live.nl>
2016-01-18 12:16:48 +0100

    index.php includes

--- views/rad.php
+++ views/rad.php
@@ -1,6 +1,8 @@
 <h1>MD2 leerlingen app</h1>
 <p>Radom leerling:</p><a href="index.php" class="randomStudent"> (shuffle)</a><br>
-<img src="img/placeholder.png" alt=""class="placeholderImg">
+<img src="img/placeholder.png" alt=""class="placeholderImg"> <div id="personTxt"><h2>Voornaam Achternaam</h2><p>
+    Voornaam is een leerling van 190 cm. Hij zit in klas MD2A en behoort tot het geslacht man.
+    Zijn ogen zijn blauw.</p></div><br>
 <?php
 /**
  * Created by PhpStorm.
