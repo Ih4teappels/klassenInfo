05f2b758db7a09fadf815c5371d7e93cc83054d6
Ih4teappels <kev10@live.nl>
2016-02-15 16:16:42 +0100

    added mediaqueries

--- views/header.php
+++ views/header.php
@@ -4,6 +4,7 @@
 <link rel="stylesheet" type="text/css" href="css/top3.css">
 <link rel="stylesheet" type="text/css" href="css/eye.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/mediaqueries.css">
 </head>
 <body>
 <div id="wrapper">
